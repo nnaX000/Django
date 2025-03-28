af18ccf18a84f1cc5f66a1afb0247354cd85c746
김나영 <kny200208@naver.com>
2025-03-28 16:47:11 +0900

    fix : signup 함수에서 로그인기능 삭제

--- user/views.py
+++ user/views.py
@@ -7,8 +7,11 @@ def signup_view(request):
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user) 
             return redirect('signup') 
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+
+
+
